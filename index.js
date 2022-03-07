5c447f0045ecc25af35828aa791b8a9616266b1f
Alana Vasconcelos <alanamvd30@gmail.com>
2022-03-06 21:50:38 -0300

    Iniciando com o JS no menu lateral

---
+++ index.js
@@ -0,0 +1,7 @@
+const botaoMenu = document.querySelector('.cabecalho__menu')   //para fazer esse menu lateral funcionar eu preciso pegar o botão e o menu lateral (no HTML)
+//se formos olhar no HTML a class do "button" é exatamente ".cabecalho__menu". O próximo passo é pegar agora o menu lateral.
+const menu = document.querySelector('.menu-lateral') 
+//se formos olhar no HTML a class do menu lateral está no "nav" com esse mesmo nome.
+
+//Mas, com isso que fizemos acima não movimentamos ainda a barra lateral para esquerda e direita. Então para isso vamos fazer mais uma classe no CSS para quando o Menu estiver ativo aparecer.
+
